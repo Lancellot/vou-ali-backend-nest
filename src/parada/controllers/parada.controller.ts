9f12a8d16e51d66b17a4da27df0c508703139826
Lancellot <assis.neto@rede.ulbra.br>
2026-05-21 15:01:46 -0300

    feat(parada): implemetação controller parada

---
+++ src/parada/controllers/parada.controller.ts
@@ -0,0 +1,49 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Param,
+    Post,
+    Put,
+} from '@nestjs/common';
+
+import { ApiTags } from '@nestjs/swagger';
+
+import { Parada } from '../entities/parada.entity';
+import { ParadaService } from '../services/parara.service';
+
+@ApiTags('Parada')
+@Controller('/paradas')
+export class ParadaController {
+
+    constructor(private readonly paradaService: ParadaService) {}
+
+    @Get('/all')
+    findAll(): Promise<Parada[]> {
+        return this.paradaService.findAll();
+    }
+
+    @Get('/:id')
+    findById(@Param('id') id: number): Promise<Parada> {
+        return this.paradaService.findById(id);
+    }
+
+    @Post('/cadastrar')
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() parada: Parada): Promise<Parada> {
+        return this.paradaService.create(parada);
+    }
+
+    @Put('/atualizar')
+    update(@Body() parada: Parada): Promise<Parada> {
+        return this.paradaService.update(parada);
+    }
+
+    @Delete('/remover/:id')
+    delete(@Param('id') id: number): Promise<void> {
+        return this.paradaService.delete(id);
+    }
+}
