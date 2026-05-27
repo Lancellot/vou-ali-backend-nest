913102a50c4a477a75da10873e41fbf65a23ce9f
Lancellot <assis.neto@rede.ulbra.br>
2026-05-27 16:44:24 -0300

    feat(data): data para produção

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { UsuarioModule } from './usuario/usuario.module';
-import { DevService } from './data/services/dev.service';
 import { ViagemModule } from './viagem/viagem.module';
 import { CidadeModule } from './cidade/cidade.module';
 import { ParadaModule } from './parada/parada.module';
 import { AtividadeModule } from './atividade/atividade.module';
 import { DespesaModule } from './despesa/despesa.module';
 import { AuthModule } from './auth/auth.module';
+import { ProdService } from './data/services/prod.service';
 
 
 @Module({
@@ -19,7 +19,7 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot(),
 
     TypeOrmModule.forRootAsync({
-      useClass: DevService,
+      useClass: ProdService,
       imports: [ConfigModule],
     }),
 
